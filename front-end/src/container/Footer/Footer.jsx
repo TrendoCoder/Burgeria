97c3856dc3d2aaae6c2d068e7e930222cb6b4281
Shamir Hussain <112416011+TrendoCoder@users.noreply.github.com>
2024-05-01 17:15:18 +0500

    Footer complete

--- front-end/src/container/Footer/Footer.jsx
+++ front-end/src/container/Footer/Footer.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { FooterOverlay, Newsletter } from '../../components';
-import './Footer.css';
-import { FiFacebook, FiTwittwr, FiInstagram } from 'react-icons/fi';
+import React from "react";
+import { FooterOverlay, Newsletter } from "../../components";
+import "./Footer.css";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { images } from "../../constants";
 const Footer = () => (
-  <div className='app__footer section__padding'>
-  <FooterOverlay />
-  <Newsletter/>
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+      <h1 className="app__footer-headtext">Contact Us</h1>
+      <p className="p__opensans">Adress of your office</p>
+      <p className="p__opensans">+92 385738393</p>
+      <p className="p__opensans">+92 536382837</p>
+      </div>
+      <div className="app__footer-links_logo">
+<img src={images.gericht} alt="logo" />
+<p className="p__opensans">The best way is to find yourself is to lose yourself in the service of others.</p>
+<img src={images.spoon} alt="spoon" className="spoon__img" style={{marginTop:'15px'}}/>
+<div className="app__footer-links_icons">
+  <FiFacebook/>
+  <FiTwitter/>
+  <FiInstagram/>
+</div>
+      </div>
+      <div className="app__footer-links_work">
+      <h1 className="app__footer-headtext">Monday - Friday</h1>
+      <p className="p__opensans">10am - 1am</p>
+      <p className="p__opensans">Saturday - Sunday</p>
+      <p className="p__opensans">12pm - 3am</p>
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2021 Burgeria. All Rights Reserved.</p>
+    </div>
   </div>
 );
 
